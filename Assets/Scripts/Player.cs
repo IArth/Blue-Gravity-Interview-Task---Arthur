43eb56cdb067de33022f38b3da6f83881e5ee784
Arthur Tavares <arth_tavares@hotmail.com>
2023-09-16 10:44:09 -0300

    Added simples player movement and interaction script made beforehand

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed=3;
+#region Made beforehand
+    //interactable script made previously in a game jam
+    List<IInteractable> interactables=new List<IInteractable>();
+    IInteractable interactable;
+    public void Interact(){
+        if(interactable!=null){
+            interactable.DeInteract();
+            interactable=null;
+            return;
+        }
+        if(interactables.Count==0)return;
+        interactable=interactables[0];
+        float distance=Vector3.Distance(transform.position,interactables[0].position);
+        for (int i = 0; i < interactables.Count; i++)
+        {
+            float d=Vector3.Distance(transform.position,interactables[i].position);
+            if(distance>d){
+                distance=d;
+                interactable=interactables[i];
+            }
+        }
+        interactable.OnInteract();
+    }
+    private void OnTriggerEnter(Collider col) {
+        IInteractable i=col.GetComponent<IInteractable>();
+        if(i!=null){
+            interactables.Add(i);
+        }
+    }
+    private void OnTriggerExit(Collider col) {
+        IInteractable i=col.GetComponent<IInteractable>();
+        if(i!=null){
+            interactables.Remove(i);
+            if(i==interactable)interactable.DeInteract();
+        }
+    }
+#endregion
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector2 inputs=new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        transform.Translate(inputs*movementSpeed*Time.deltaTime);
+    }
+}
