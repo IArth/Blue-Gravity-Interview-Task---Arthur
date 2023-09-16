049b7151010d2db43ff2c0e4b13e9a53d3c1f496
Arthur Tavares <arth_tavares@hotmail.com>
2023-09-16 15:47:42 -0300

    Shopt screen creates the buttons in the right places, fills the buttons with information and works for buying/equipping pieces

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -7,14 +7,15 @@ using Customization;
 public class ShopManager : MonoBehaviour,IInteractable
 {
     [SerializeField]
-    private RectTransform buttomPrefab;
+    private ShopButton buttomPrefab;
     [SerializeField]
     private CustomizationScriptable scriptable;
     [SerializeField]
     private RectTransform shopPanel;
     [SerializeField]
     private Player player;
-    private Vector3 shopPanelScale;
+    private List<ShopButton> buttons=new List<ShopButton>();
+    private Vector3 shopPanelScale=Vector3.one;
     public Vector3 position => transform.position;
 
     public void OnInteract()
@@ -28,26 +29,48 @@ public class ShopManager : MonoBehaviour,IInteractable
     }
     private void BuildMenu(){
         CustomPiece[] pieces=scriptable.GetPieces();
+        buttomPrefab.gameObject.SetActive(true);
+        Vector2 size=buttomPrefab.size;
+        int count=0;
         foreach (CustomPiece piece in pieces)
         {
-            RectTransform buttonTransform=Instantiate(buttomPrefab);
-            Button button=buttonTransform.GetComponent<Button>();
-            button.onClick.AddListener(()=>BuyPiece(piece));
-            button.interactable=piece.price<100;
-            Text[] texts=buttonTransform.GetComponentsInChildren<Text>();
-            texts[0].text=piece.name;
-            texts[1].text=piece.price.ToString();
-            buttonTransform.GetComponentInChildren<Image>().sprite=piece.icon;
+            ShopButton button=Instantiate(buttomPrefab,shopPanel);
+            button.onClick=()=>BuyPiece(piece);
+            button.interactable=player.Obtained(piece.name) || piece.price<=player.coins;
+            button.pieceName=piece.name;
+            button.piecePrice=player.Obtained(piece.name)?"Equip":piece.price.ToString();
+            button.icon=piece.icon;
+            button.position=new Vector2(count%4*size.x,count/4*size.y);
+            count++;
+            buttons.Add(button);
         }
+        buttomPrefab.gameObject.SetActive(false);
     }
     private void BuyPiece(CustomPiece piece){
-        Debug.Log("Buying "+piece.name);
+        if(!player.Obtained(piece.name)){
+            player.Obtain(piece.name);
+            player.Spend(piece.price);
+            RefreshPrices();
+            Debug.Log("Buying "+piece.name);
+        }
+        Debug.Log("Equiping "+piece.name);
         player.EquipPiece(piece.pieceType,piece.animatorController);
+    }
+    private void RefreshPrices(){
+        CustomPiece[] pieces=scriptable.GetPieces();
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            buttons[i].interactable=player.Obtained(pieces[i].name) || pieces[i].price<=player.coins;
+            buttons[i].pieceName=pieces[i].name;
+            buttons[i].piecePrice=player.Obtained(pieces[i].name)?"Equip":pieces[i].price.ToString();
+        }
     }
     void Start()
     {
+        BuildMenu();
     }
     void Update()
     {
+        shopPanel.localScale=Vector3.MoveTowards(shopPanel.localScale,shopPanelScale,Time.deltaTime/1f);
     }
 }
