e917a1a2d482761497b7fb435681a0819a803682
Arthur Tavares <arth_tavares@hotmail.com>
2023-09-16 11:58:49 -0300

    Added Scriptable for custom pieces for player layers

---
+++ Assets/Scripts/CustomizationScriptable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Customization
+{
+    [CreateAssetMenu(fileName = "CustomizationScriptable", menuName = "Customization/CustomizationScriptable", order = 0)]
+    public class CustomizationScriptable : ScriptableObject {
+        [SerializeField]
+        private CustomPiece[] pieces;
+        public CustomPiece GetPiece(int i){
+            if(i<0 || i>=pieces.Length){
+                Debug.LogError("Outside of range:"+i);
+                return new CustomPiece();
+            }
+            return pieces[i];
+        }
+        public CustomPiece[] GetPieces()=>pieces;
+    }
+    [System.Serializable]
+    public struct CustomPiece
+    {
+        public string name;
+        public Sprite icon;
+        public RuntimeAnimatorController animatorController;
+        public CustomType pieceType;
+    }
+    public enum CustomType
+    {
+        Character,Cloth,Hair,Hat
+    }
+}
